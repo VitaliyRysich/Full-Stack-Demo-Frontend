d9f19a1e2e1f21a1b6581b06fefd9d8286d05166
Vitalii <vitaliy.rysich@gmail.com>
2021-02-04 23:04:10 +0200

    securing app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { ListTodosComponent } from './component/list-todos/list-todos.component'
 import { LoginComponent } from './component/login/login.component';
 import { LogoutComponent } from './component/logout/logout.component';
 import { WelcomeComponent } from './component/welcome/welcome.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
+  { path: '', component: LoginComponent}, //canActivate, RouteGuardService
   { path: 'login', component: LoginComponent},
-  { path: 'welcome/:name', component: WelcomeComponent},
-  { path: 'todos', component: ListTodosComponent},
-  { path: 'logout', component: LogoutComponent},
+  { path: 'welcome/:name', component: WelcomeComponent, canActivate:[RouteGuardService]},
+  { path: 'todos', component: ListTodosComponent, canActivate:[RouteGuardService]},
+  { path: 'logout', component: LogoutComponent, canActivate:[RouteGuardService]},
 
   { path: '**', component: ErrorComponent}
 ];
