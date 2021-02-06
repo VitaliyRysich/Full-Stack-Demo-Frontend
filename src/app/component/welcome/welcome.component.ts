6b7ac4f753bed25c6bbd80456ff8f39d74864a8d
Vitalii <vitaliy.rysich@gmail.com>
2021-02-06 14:48:37 +0200

    Connect Angular with RestApi

--- src/app/component/welcome/welcome.component.ts
+++ src/app/component/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from 'src/app/service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -8,12 +9,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WelcomeComponent implements OnInit {
 
+  welcomeMessageFromService: string;
   name = '';
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService) { }
 
   ngOnInit(): void {
     this.name = this.route.snapshot.params['name']
+  }
+
+  getWelcomeMessage(){
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+  }
+
+
+  handleSuccessfulResponse(response){
+    this.welcomeMessageFromService = response.message;
+  }
+
+  handleErrorResponse(error) {
+    this.welcomeMessageFromService = error.error.message;
   }
 
 }
